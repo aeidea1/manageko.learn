fb238c6f1fd9b2386204b18cc43039a030858695
aeideal <sembaevaman213@gmail.com>
2026-03-16 13:48:16 +0600

    add vite api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Используем localhost, так как бэкенд запущен именно на нём
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 // Автоматически добавляем токен (если он есть) к каждому запросу
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
