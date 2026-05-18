1e115785f4f6aada55b739e7ffd539a68824bfdc
aeideal <sembaevaman213@gmail.com>
2026-05-18 17:46:34 +0600

    PROD V 7.0

--- server/prisma/migrations/20260518112453_add_profile_fields/migration.sql
+++ server/prisma/migrations/20260518112453_add_profile_fields/migration.sql
@@ -1,11 +1,9 @@
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "bio" TEXT,
-ADD COLUMN     "coverColor" TEXT DEFAULT '#0056D2',
-ADD COLUMN     "github" TEXT,
-ADD COLUMN     "resetToken" TEXT,
-ADD COLUMN     "resetTokenExpiry" TIMESTAMP(3),
-ADD COLUMN     "telegram" TEXT,
-ADD COLUMN     "vk" TEXT;
+ALTER TABLE "User" ADD COLUMN "bio" TEXT,
+ADD COLUMN "coverColor" TEXT DEFAULT '#0056D2',
+ADD COLUMN "github" TEXT,
+ADD COLUMN "telegram" TEXT,
+ADD COLUMN "vk" TEXT;
 
 -- CreateTable
 CREATE TABLE "UserActivity" (
@@ -17,44 +15,8 @@ CREATE TABLE "UserActivity" (
     CONSTRAINT "UserActivity_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "PracticeDocument" (
-    "id" SERIAL NOT NULL,
-    "lessonId" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "size" TEXT,
-
-    CONSTRAINT "PracticeDocument_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Comment" (
-    "id" SERIAL NOT NULL,
-    "lessonId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "text" TEXT NOT NULL,
-    "edited" BOOLEAN NOT NULL DEFAULT false,
-    "parentId" INTEGER,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "UserActivity_userId_date_key" ON "UserActivity"("userId", "date");
 
 -- AddForeignKey
 ALTER TABLE "UserActivity" ADD CONSTRAINT "UserActivity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "PracticeDocument" ADD CONSTRAINT "PracticeDocument_lessonId_fkey" FOREIGN KEY ("lessonId") REFERENCES "Lesson"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_lessonId_fkey" FOREIGN KEY ("lessonId") REFERENCES "Lesson"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
